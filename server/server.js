9ea19f874d9f7168f187af58e07df4636c0b8a01
Den Ezra Segunial <den.segunial@gmail.com>
2023-01-11 07:41:50 +0800

    Update server pathing

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 const express = require('express');
-const dbConfig = require('./config/dbConfig');
-const userRoute = require('./routes/user');
-const adminRoute = require('./routes/admin');
-const doctorRoute = require('./routes/doctor');
+const dbConfig = require('./api/config/dbConfig');
+const userRoute = require('./api/routes/user');
+const adminRoute = require('./api/routes/admin');
+const doctorRoute = require('./api/routes/doctor');
 const path = require('path');
 const cors = require('cors');
 
@@ -17,16 +17,16 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/user', userRoute);
-app.use('/admin', adminRoute);
-app.use('/doctor', doctorRoute);
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/doctor', doctorRoute);
 
 
-app.use((req, res, next) =>
-{
-    res.status(404).send(
-        "<h1>Page not found on the server</h1>")
-})
+// app.use((req, res, next) =>
+// {
+//     res.status(404).send(
+//         "<h1>Page not found on the server</h1>")
+// })
 const port = process.env.PORT || 5000;
 
 // app.use('/', express.static('client/build'));
