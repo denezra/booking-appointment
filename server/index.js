274007403d12074bdf5c08641f62c01cde517927
Den Ezra Segunial <den.segunial@gmail.com>
2023-01-11 10:22:41 +0800

    Update server

--- server/index.js
+++ server/index.js
@@ -11,20 +11,12 @@ const app = express();
 app.use(cors());
 
 app.use("/api/v1", routes)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-// app.use((req, res, next) =>
-// {
-//     res.status(404).send(
-//         "<h1>Page not found on the server</h1>")
-// })
 const port = process.env.PORT || 5000;
 
 const server = http.createServer(app);
-
-// app.use('/', express.static('client/build'));
-// app.get('*', (req, res) => {
-// 	res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-// });
 
 server.listen(port, () => console.log(`Node server started at port ${port}`));
 
